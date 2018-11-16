0717fe6352265e8941b7f30983c1b75d4dc1fae2
JaretWright <hkkA223>
2018-11-16 06:24:10 -0500

    Initial upload

---
+++ src/w18comp1011w11prepthreads/W18COMP1011W11PrepThreads.java
@@ -0,0 +1,18 @@
+package w18comp1011w11prepthreads;
+
+/**
+ *
+ * @author JWright
+ */
+public class W18COMP1011W11PrepThreads
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
